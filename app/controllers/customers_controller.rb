44e3a269e4075bc48f293d0422de1f1858a76ed8
Devpe007 <anddev007pe@gmail.com>
2024-07-12 20:31:06 -0400

    feature: update customer method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
   before_action :authorize
+  before_action :load_customer, only: [:update]
 
   def create
     @customer = Customer.create(customer_params)
@@ -12,7 +13,19 @@ class CustomersController < ApplicationController
     end
   end
 
+  def update
+    if @customer.update(customer_params)
+      render json: @customer, status: :accepted
+    else
+      render json: @customer.erros, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def load_customer
+    @customer = Customer.find(params[:id])
+  end
 
   def customer_params
     params.permit(:name, :email, :mobile_number, :address, :description)
